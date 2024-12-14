36de8eae00d999189b2aad5148c13327b6dfdabc
Supremacy <3118654731@qq.com>
2024-12-15 00:00:38 +0800

    1.2.0 优化打包格式;修复一些已知问题

--- rollup.config.js
+++ rollup.config.js
@@ -5,20 +5,24 @@ export default [
     {
         input: 'src/index.ts',  // 输入文件
         output: {
-            file: 'dist-esm/index.js',  // 输出文件
+            file: 'dist/dist-esm/index.js',  // 输出文件
             format: 'esm',  // 输出格式为 ESM
         },
-        plugins: [typescript()],
+        plugins: [typescript({
+            useTsconfigDeclarationDir:true
+        })],
     },
 
     // CJS 输出配置
     {
         input: 'src/index.ts',
         output: {
-            dir: 'dist-cjs',  // 输出到 dist-cjs 目录
+            dir: 'dist/dist-cjs',  // 输出到 dist-cjs 目录
             format: 'cjs',  // 输出格式为 CJS
             entryFileNames: '[name].cjs',  // 使用 .cjs 后缀
         },
-        plugins: [typescript()],
+        plugins: [typescript({
+            useTsconfigDeclarationDir: true
+        })],
     },
 ]
